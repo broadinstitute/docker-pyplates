5a6846eedc4a4d1662d2d69d06283bb614ad61ae
coreone <teixeira@broadinstitute.org>
2015-12-12 15:32:25 -0500

    * Add bash to image * Update mkcf.sh to get rid of some bugs and to clean it up a bit * Make mkcf.sh work better on Mac

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:3.2
 
 RUN apk update && \
-    apk add python py-pip wget && \
+    apk add bash python py-pip wget && \
     pip install cfn-pyplates && \
     mkdir /cfn && \
     rm -rf /tmp/* && \
--- mkcf.sh
+++ mkcf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_IMAGE='pyplates:latest'
+DOCKER_IMAGE='broadinstitute/pyplates:latest'
 DOCKER_SOCKET='/var/run/docker.sock'
 SUDO=
 
@@ -13,35 +13,32 @@ if [ "$TERM" != "dumb" ] ; then
     TTY='-it'
 fi
 
-if [ ! -w "${DOCKER_SOCKET}" ];
-then
-    SUDO='sudo'
+if [ `uname -s` != "Darwin" ]; then
+    if [ ! -w "${DOCKER_SOCKET}" ]; then
+	SUDO='sudo'
+    fi
 fi
 
-if [ -z "$1" ];
-    then
+if [ -z "$1" ]; then
     usage
     exit 1
 fi
 
-if [ -z "$2" ];
-    then
+if [ -z "$2" ]; then
     usage
     exit 2
 fi
 
 TEMPLATE_DIR="$( cd -P "$1" && pwd )"
-if [ ! -d "${TEMPLATE_DIR}" ];
-    then
+if [ ! -d "${TEMPLATE_DIR}" ]; then
     echo "Directory `${TEMPLATE_DIR}` does not exist...exiting."
     exit 3
 fi
 
 templatefile="${TEMPLATE_DIR}/template.py"
-if [ ! -f "${templatefile}" ];
-    then
+if [ ! -f "${templatefile}" ]; then
     echo "Template file `${templatefile}` does not exist...exiting."
     exit 4
 fi
 
-$SUDO docker run $TTY --rm -v $TEMPLATE_DIR:/cfn pyplates cfn_py_generate /cfn/template.py > $2
+$SUDO docker run $TTY --rm -v $TEMPLATE_DIR:/cfn $DOCKER_IMAGE cfn_py_generate /cfn/template.py > $2
